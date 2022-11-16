fee8cf7e5a12b40336888be627d9eafbe3f37f40
dwl21 <nggus5@naver.com>
2022-11-16 16:50:18 +0900

    11659

--- baekjoon/class3/11659.py
+++ baekjoon/class3/11659.py
@@ -1,8 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+
 N, M = map(int, input().split())
 numbers = list(map(int, input().split()))
+
+maximum = 0
+answer = [0]
+for i in numbers:
+    maximum += i
+    answer.append(maximum)
+
 for _ in range(M):
     start, end = map(int, input().split())
     if start == end:
-        print(numbers[start-1])
+        print(answer[end] - answer[start - 1])
     else:
-        print(sum(numbers[start-1:end]))
+        print(answer[end] - answer[start - 1])
