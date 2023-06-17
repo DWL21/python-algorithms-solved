0e604310797d4227f0668717013c64ec74453511
dwl21 <nggus5@gmail.com>
2023-06-16 22:51:06 -0400

    2231 solved

---
+++ roka/src/2231.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solution(n: int) -> int:
+    for i in range(1, n):
+        if sum(map(int, str(i))) + i == n:
+            return i
+    return 0
+
+
+N = int(input())
+print(solution(N))
