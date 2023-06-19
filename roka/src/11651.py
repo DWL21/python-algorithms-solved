5c396d4038ab8c0f6e1f6734791dcb8fbb4c9d3f
dwl21 <nggus5@gmail.com>
2023-06-19 07:20:46 -0400

    11651 solved

--- roka/src/11651.py
+++ roka/src/11651.py
@@ -6,6 +6,6 @@ coordinates = []
 N = int(input())
 for _ in range(N):
     coordinates.append(tuple(map(int, input().split())))
-coordinates.sort(key= lambda x: (x[0], x[1]))
+coordinates.sort(key= lambda x: (x[1], x[0]))
 for x, y in coordinates:
     print(f"{x} {y}")
