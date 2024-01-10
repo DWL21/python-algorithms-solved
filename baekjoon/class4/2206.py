dfe6fa193e09ba4ed80a8df7891b4646667ea386
Hyun NamGung <nggus5@naver.com>
2024-01-10 13:42:33 +0900

    Update 2206.py_solved

--- baekjoon/class4/2206.py
+++ baekjoon/class4/2206.py
@@ -3,7 +3,7 @@ from collections import deque
 N, M = map(int, input().split())
 board = []
 for _ in range(N):
-	board.append(input())
+	board.append(list(map(int, input())))
 
 directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 INF = int(1e9)
@@ -11,21 +11,22 @@ INF = int(1e9)
 
 def make_routes(now, board, N, M):
     routes = []
+    x, y, distance, breaking = now
     for l, m in directions:
-            x, y, distance, breaking = now
-            x_goto = x + l
-            y_goto = y + m
-            if 0 <= x_goto and x_goto < N and 0 <= y_goto and y_goto < M:
-                if board[x_goto][y_goto] == 0:
-                    routes.append((x_goto, y_goto, distance + 1, True))
-                    continue
-                if breaking:
-                    routes.append((x_goto, y_goto, distance + 1, False))
+        x_goto = x + l
+        y_goto = y + m
+        if 0 > x_goto or x_goto >= N or 0 > y_goto or y_goto >= M:
+            continue
+        if board[x_goto][y_goto] and breaking:
+            routes.append((x_goto, y_goto, distance + 1, False))
+            continue
+        if not board[x_goto][y_goto]:
+            routes.append((x_goto, y_goto, distance + 1, breaking))
     return routes
 
 
-visited = [[False] * N for _ in range(M)]
-breaking_visited = [[False] * N for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
+breaking_visited = [[False] * M for _ in range(N)]
 
 answer = INF
 start = (0, 0, 0, True)
@@ -33,7 +34,6 @@ queue = deque([start])
 
 while queue:
     now = queue.popleft()
-    print(now)
     x, y, distance, breaking = now
     if (x, y) == (N - 1, M - 1):
         answer = min(answer, distance)
@@ -47,5 +47,8 @@ while queue:
     queue.extend(make_routes(now, board, N, M))
 
 
-print(answer)
-	
+if answer < INF:
+    print(answer + 1)
+else:
+    print(-1)
+    
