fcc974de3ad649eb5b1700c1c4c79ac776ced2af
Hyun NamGung <nggus5@gmail.com>
2024-01-20 15:05:44 +0900

    Add files via upload

--- baekjoon/class4/12865_2.py
+++ baekjoon/class4/12865_2.py
@@ -5,13 +5,13 @@ stuffs = []
 for _ in range(N):
     stuffs.append(tuple(map(int, input().split())))
 
-dp = [[0] * len(stuffs) for _ in range(K + 1)]
-for i in range(len(stuffs)):
-    w, v = stuffs[i]
-    for j in range(K + 1):
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    w, v = stuffs[i - 1]
+    for j in range(1, K + 1):
         if w > j:
-            dp[j][i] = dp[j][i - 1]
+            dp[i][j] = dp[i - 1][j]
         else:
-            dp[j][i] = max(dp[j][i - 1], v + dp[j - w][i - 1])
-print(dp[-1][-1])
+            dp[i][j] = max(dp[i - 1][j], v + dp[i - 1][j - w])
+print(dp[N][K])
 
